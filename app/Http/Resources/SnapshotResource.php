b62b463a113bca17796f12c6f2e907cf443c55e5
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-10 12:09:17 -0600

    fix snapshot resource class

--- app/Http/Resources/SnapshotResource.php
+++ app/Http/Resources/SnapshotResource.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
  * @property mixed id
+ * @property string html
+ * @property int status_code
  * @property string created_at
  * @property string updated_at
  * @property string scan_interval
@@ -17,6 +19,17 @@ class SnapshotResource extends JsonResource
 
     public function toArray(Request $request): array
     {
-        return [];
+        return [
+            'id' => $this->id,
+            'site' => SiteResource::make($this->whenLoaded('site')),
+
+            'html' => $this->html,
+            'status_code' => $this->status_code,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            'scan_interval' => $this->scan_interval,
+            'last_scanned_at' => $this->last_scanned_at,
+        ];
     }
 }
