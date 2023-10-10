de78121ca358a701567c4aa53b5e3a676e230c95
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-10 10:31:54 -0600

    implements user resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed id
+ * @property string name
+ * @property string email
+ * @property string created_at
+ * @property string updated_at
+ */
+class UserResource extends JsonResource
+{
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
