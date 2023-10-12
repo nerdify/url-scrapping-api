70b398b65ae246951a005d7d5c4a21ae9520dd51
aguilarkevin <kevinaguilar0813@gmail.com>
2023-10-12 10:10:42 -0600

    Fix TakeSnapshot Job

--- app/Jobs/TakeSiteSnapshot.php
+++ app/Jobs/TakeSiteSnapshot.php
@@ -11,6 +11,8 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Browsershot\Browsershot;
 use Throwable;
 
@@ -23,18 +25,19 @@ class TakeSiteSnapshot implements ShouldQueue
         //
     }
 
-
     public function handle(): void
     {
+        logger('TakeSiteSnapshot job started');
+
         try {
             DB::beginTransaction();
 
             $response = Http::get($this->site->url);
 
-            $snapshotDate = now()->format('Ymd-His');
+            logger($response->status());
 
             $this->site->update([
-                'last_scanned_at' => $snapshotDate,
+                'last_scanned_at' => now()->toDateTime(),
             ]);
 
             /** @var Snapshot $snapshot */
@@ -43,27 +46,53 @@ class TakeSiteSnapshot implements ShouldQueue
             ]);
 
             if ($response->successful()) {
-                $pathToSnapshot = storage_path("app/public/snapshots/{$this->site->id}-{$snapshotDate}.png");
 
-                $browserShot = Browsershot::url($this->site->url);
+                if (!Storage::disk('public')->directoryExists("snapshots/site-{$this->site->id}")) {
+                    Storage::disk('public')->makeDirectory("snapshots/site-{$this->site->id}");
+                }
+
+                $filename = "{$this->site->id}-".now()->format('Ymd-His').".png";
+                $pathToSnapshot = Storage::disk('public')->path("snapshots/site-{$this->site->id}");
+
+                logger($pathToSnapshot);
+
+                $browserShot = Browsershot::url($this->site->url)
+                    ->newHeadless()
+                    ->setChromePath('/usr/bin/google-chrome'));
 
                 $snapshot->update([
                     'html' => $browserShot->bodyHtml(),
                 ]);
 
                 $browserShot->setScreenshotType('png', 100)
-                    ->save($pathToSnapshot);
+                    ->save($pathToSnapshot.'/'.$filename);
 
-                $snapshot->addMedia($pathToSnapshot)
+                $snapshot->addMedia($pathToSnapshot.'/'.$filename)
                     ->withResponsiveImages()
                     ->toMediaCollection();
             }
 
             DB::commit();
+
+//            $this->release($this->site->scan_interval * 60);
+
+            TakeSiteSnapshot::dispatch($this->site->fresh())
+                ->onQueue('snapshots')
+                ->delay(now()->addMinutes($this->site->scan_interval));
+
+            logger('TakeSiteSnapshot job finished, next job will be released in ' . $this->site->scan_interval * 60 . ' seconds');
+
         } catch (Throwable $e) {
             logger($e->getMessage());
-
             DB::rollBack();
+
+//            $this->release($this->site->scan_interval * 60);
+
+            TakeSiteSnapshot::dispatch($this->site->fresh())
+                ->onQueue('snapshots')
+                ->delay(now()->addMinutes($this->site->scan_interval));
+
+            logger('TakeSiteSnapshot job failed, next job will be released in ' . $this->site->scan_interval * 60 . ' seconds');
         }
 
     }
